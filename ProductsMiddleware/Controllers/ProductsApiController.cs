f867f449d8303598d7a80c5693c3dbc6a4ef31cc
tonileo <mdtonileo@gmail.com>
2024-06-20 16:54:37 +0200

    Implemented GetProductsFilterCategoryAndPrice

--- ProductsMiddleware/Controllers/ProductsApiController.cs
+++ ProductsMiddleware/Controllers/ProductsApiController.cs
@@ -29,7 +29,7 @@ namespace ProductsMiddleware.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public async Task<IActionResult> GetProduct (int id)
+        public async Task<IActionResult> GetProduct(int id)
         {
             var client = httpClientFactory.CreateClient();
 
@@ -38,6 +38,38 @@ namespace ProductsMiddleware.Controllers
 
             var responseBody = await response.Content.ReadFromJsonAsync<Product>();
             return Ok(responseBody);
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetProductsFilterCategoryAndPrice([FromQuery] string filterCategory, [FromQuery] decimal filterMinPrice, [FromQuery] decimal filterMaxPrice)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://dummyjson.com/products/");
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadFromJsonAsync<ProductList>();
+
+            if (responseBody?.Products == null)
+            {
+                return NotFound();
+            }
+
+            var filteredProducts = responseBody.Products;
+
+            if (!string.IsNullOrEmpty(filterCategory) || decimal.IsPositive(filterMinPrice) || decimal.IsPositive(filterMaxPrice))
+            {
+                filteredProducts = filteredProducts
+                    .Where(p => p.Category.Contains(filterCategory, StringComparison.OrdinalIgnoreCase) && p.Price > filterMinPrice && p.Price < filterMaxPrice)
+                    .ToList();
+            }
+
+            if (!filteredProducts.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(filteredProducts);
         }
     }
 }
