671f77ec80bab3f72534fd66c82963344224821c
tonileo <mdtonileo@gmail.com>
2024-06-21 20:15:19 +0200

    Added xUnit test environment

---
+++ Products_UnitTests/Controller/AuthApiController_Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsMiddleware_UnitTests.Controller
+{
+    public class AuthApiController_Tests
+    {
+
+    }
+}
